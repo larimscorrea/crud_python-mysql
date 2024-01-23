05214a78f026cba3f08afcf649f77bb66d58ebd6
unknown <info@NOTE-03.cgm.fortaleza.ce.gov.br>
2024-01-23 11:02:51 -0300

    consertei o botão

--- app/views.py
+++ app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['carro'] = 'Fiat Uno'
+    return render(request, 'index.html', data)
