c3e58d41209176a58899b3a97f9312fc7d0dfd84
Larissa Corrêa <larimscorrea@gmail.com>
2024-01-24 17:49:50 -0300

    solução do chatgpt, ainda sem funcionar

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import PersonForm
 from app.models import Person
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -15,12 +18,18 @@ def form(request):
     return render(request, 'form.html', data)
 
 def create(request):
-    form = PersonForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        return redirect('home.html')
-    
-def view(request, pk):
-    data = {}
-    data['db'] = Person.objects.get(pk=pk)
-    return render(request, 'view.html')
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registro adicionado com sucesso')
+            ## return redirect('home')
+            print("Registro adicionado com sucesso!")
+        ## return render(request, 'form.html', {'form': PersonForm()})
+            
+        print("Erro ao adicionar registro:", form.errors)  # Adicione esta linha para depuração
+        return HttpResponse("Erro ao adicionar o registro.")
+## def view(request, pk):
+##    data = {}
+##    data['db'] = Person.objects.get(pk=pk)
+##    return render(request, 'view.html') 
