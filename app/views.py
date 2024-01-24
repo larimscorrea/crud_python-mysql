f5ef72e55f45f7daa7f691abae529f14766ba520
Larissa Corrêa <larimscorrea@gmail.com>
2024-01-24 00:23:35 -0300

    [200~resolvi o problema no backend, agora preciso resolver que o botão de salvar não está salvando no banco de dados os dados

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from . forms import PersonForm
+from app.models import Person
+from django.shortcuts import redirect
+
 # Create your views here.
 
 def home(request):
+    data = {}
+    data['db'] = Person.objects.all()
     return render(request, 'index.html')
 
 def form(request): 
@@ -9,3 +15,9 @@ def form(request):
     data['form'] = PersonForm()
     return render(request, 'form.html', data)
 
+def create(request):
+    form = PersonForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home.html')
+    
--- project/urls.py
+++ project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, form
+from app.views import home, form, create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
         path('', home),
         path('form/', form),
+        path('create/', create),
 
 ]
